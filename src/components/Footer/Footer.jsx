ae404c9798520d2087503d74e429224662182664
Debarup <87480898+debarup24@users.noreply.github.com>
2024-10-22 12:44:00 +0530

    CryptoTracker V1

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <p>
+        Created by{" "}
+        <a href="https://www.linkedin.com/in/debarup-s/" target="_blank">
+          Debarup
+        </a>
+        <br /> copyright @ 2024 || CryptoTracker - All Right Reserved
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
